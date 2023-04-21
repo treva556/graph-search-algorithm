8e472249561d30ac8fa4d4992f1350c031b56fbb
treva556 <marktreva92@gmail.com>
2023-04-21 04:17:51 +0300

    airports & routes

--- graph.js
+++ graph.js
@@ -1,5 +1,18 @@
 const airports = 'PHX BKK OKC JFK LAX MEX EZE HEL LOS PAL LIM'.split('  ');
 
 const routes = [
-    []
-]
+    ['PHX', 'LAX'],
+    ['PHX', 'JFK'],
+    ['JFK', 'OKC'],
+    ['JFK', 'HEL'],
+    ['JFK', 'LOS'],
+    ['MEX', 'LAX'],
+    ['MEX', 'BKK'],
+    ['MEX', 'LIM'],
+    ['MEX', 'EZE'],
+    ['LIM', 'BKK'],
+];
+
+const adjacencyList = new Map();
+
+function addNode()
